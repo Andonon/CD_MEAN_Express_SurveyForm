53a975e600ec987a65c60be8d1b532ab01499076
Troy Center <troycenter1@gmail.com>
2017-07-09 14:29:11 -0700

    added two routes and tested. submitted assignments

--- app.js
+++ app.js
@@ -8,11 +8,24 @@ app.set('view engine', 'ejs');
 app.use(session({secret: 'lkjasldkjlaksjd098324lkjasdl0834'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/static/'));
-app.set('views', __dirname + '/views/');
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){ 
-    res.send("Hello Survey Form");
+    res.render('index');
+});
+
+app.post('/', function(req, res){
+    res.redirect('/')
+});
+
+app.post('/result', function(req, res){
+    postData = req.body
+    console.log("*****************************************************")
+    console.log(req.body)
+    console.log("*****************************************************")
+    res.render('result', postData)
 });
 
 port=5000;
